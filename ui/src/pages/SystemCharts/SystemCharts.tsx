699afaf6b0fe52d8fb8002573196a1e7f068f3c6
Mateusz Biel <matbiel@gmail.com>
2026-07-06 18:23:49 +0200

    Implement date picker

--- ui/src/pages/SystemCharts/SystemCharts.tsx
+++ ui/src/pages/SystemCharts/SystemCharts.tsx
@@ -1,13 +1,16 @@
 import { usePrometheusQueryRange } from "../../api/hooks/usePrometheusQueryRange.ts";
 import { type ChangeEvent, useState } from "react";
-import dayjs, { type ManipulateType } from "dayjs";
+import dayjs, { Dayjs, type ManipulateType } from "dayjs";
 import { ThermalChart } from "./components/ThermalChart.tsx";
+import { DataRangeDropdown } from "./components/DataRangeDropdown.tsx";
+import { Calendar } from "lucide-react";
 
-type DataRange = "5m" | "30m" | "1h" | "5h";
+type DataRange = "5m" | "30m" | "1h" | "5h" | "Custom";
 type TimeWindow = "30s" | "1m" | "5m";
 type AggregationFunction = "avg_over_time" | "max_over_time";
 
-interface ChartConfig {
+interface ChartState {
+  start: dayjs.Dayjs;
   end: dayjs.Dayjs;
   range: DataRange;
   aggFunc: AggregationFunction;
@@ -16,7 +19,7 @@ interface ChartConfig {
 
 type ValueAndUnit = [number, ManipulateType];
 
-const toValueAndUnit = (range: DataRange): ValueAndUnit => {
+const toValueAndUnit = (range: DataRange): ValueAndUnit | null => {
   switch (range) {
     case "5m":
       return [5, "minutes"];
@@ -27,30 +30,44 @@ const toValueAndUnit = (range: DataRange): ValueAndUnit => {
     case "5h":
       return [5, "hours"];
   }
+  return null;
+};
+
+const calcStart = (end: Dayjs, range: DataRange): Dayjs | null => {
+  const r = toValueAndUnit(range);
+  if (r === null) {
+    return null;
+  }
+  const [value, unit] = r;
+  return end.subtract(value, unit);
 };
 
 export const SystemCharts = () => {
-  const [chartConfig, setChartConfig] = useState<ChartConfig>({
-    end: dayjs(),
-    range: "30m",
-    aggFunc: "avg_over_time",
-    window: "30s",
+  const [chartConfig, setChartConfig] = useState<ChartState>(() => {
+    const end = dayjs();
+    return {
+      start: end.subtract(30, "minutes"),
+      range: "30m",
+      aggFunc: "avg_over_time",
+      window: "30s",
+      end,
+    };
   });
-  const valueAndUnit = toValueAndUnit(chartConfig.range);
   const { data, isLoading } = usePrometheusQueryRange({
     query: `${chartConfig.aggFunc}(node_thermal_zone_temp{job="kubernetes-pods"}[${chartConfig.window}])`,
-    start: chartConfig.end
-      .subtract(valueAndUnit[0], valueAndUnit[1])
-      .toISOString(),
+    start: chartConfig.start.toISOString(),
     end: chartConfig.end.toISOString(),
     step: chartConfig.window,
   });
 
   const handleRangeChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const end = dayjs();
+    const newRange = event.target.value as DataRange;
     setChartConfig({
       ...chartConfig,
-      range: event.target.value as DataRange,
-      end: dayjs(),
+      start: calcStart(end, newRange)!,
+      range: newRange,
+      end,
     });
   };
 
@@ -58,7 +75,6 @@ export const SystemCharts = () => {
     setChartConfig({
       ...chartConfig,
       window: event.target.value as TimeWindow,
-      end: dayjs(),
     });
   };
 
@@ -66,126 +82,153 @@ export const SystemCharts = () => {
     setChartConfig({
       ...chartConfig,
       aggFunc: event.target.value as AggregationFunction,
-      end: dayjs(),
     });
   };
 
   return (
-    <div className="flex flex-col">
-      <div className="flex flex-row">
-        <div className="join">
-          <label className="input rounded-l-lg border-0 font-bold">Range</label>
-          <input
-            className="join-item btn"
-            type="radio"
-            name="data-range"
-            value="5m"
-            aria-label="5m"
-            disabled={isLoading}
-            checked={chartConfig.range === "5m"}
-            onChange={handleRangeChange}
-          />
-          <input
-            className="join-item btn"
-            type="radio"
-            name="data-range"
-            value="30m"
-            aria-label="30m"
-            disabled={isLoading}
-            checked={chartConfig.range === "30m"}
-            onChange={handleRangeChange}
-          />
-          <input
-            className="join-item btn"
-            type="radio"
-            name="data-range"
-            value="1h"
-            aria-label="1h"
-            disabled={isLoading}
-            checked={chartConfig.range === "1h"}
-            onChange={handleRangeChange}
-          />
-          <input
-            className="join-item btn"
-            type="radio"
-            name="data-range"
-            value="5h"
-            aria-label="5h"
-            disabled={isLoading}
-            checked={chartConfig.range === "5h"}
-            onChange={handleRangeChange}
-          />
+    <>
+      <div className="flex flex-col">
+        <div className="flex flex-row">
+          <div className="join">
+            <label className="input rounded-l-lg border-0 font-bold">
+              Range
+            </label>
+            <input
+              className="join-item btn"
+              type="radio"
+              name="data-range"
+              value="5m"
+              aria-label="5m"
+              disabled={isLoading}
+              checked={chartConfig.range === "5m"}
+              onChange={handleRangeChange}
+            />
+            <input
+              className="join-item btn"
+              type="radio"
+              name="data-range"
+              value="30m"
+              aria-label="30m"
+              disabled={isLoading}
+              checked={chartConfig.range === "30m"}
+              onChange={handleRangeChange}
+            />
+            <input
+              className="join-item btn"
+              type="radio"
+              name="data-range"
+              value="1h"
+              aria-label="1h"
+              disabled={isLoading}
+              checked={chartConfig.range === "1h"}
+              onChange={handleRangeChange}
+            />
+            <input
+              className="join-item btn"
+              type="radio"
+              name="data-range"
+              value="5h"
+              aria-label="5h"
+              disabled={isLoading}
+              checked={chartConfig.range === "5h"}
+              onChange={handleRangeChange}
+            />
+            <button
+              className={`join-item btn ${chartConfig.range === "Custom" ? "btn-primary" : ""}`}
+              disabled={isLoading}
+              popoverTarget="custom-dr-popover"
+              popoverTargetAction="show"
+              style={{
+                anchorName: "--custom-dr-anchor",
+              }}
+            >
+              Custom
+              <Calendar />
+            </button>
+          </div>
+          <div className="join ml-10">
+            <label className="input rounded-l-lg border-0 font-bold">
+              Window
+            </label>
+            <input
+              className="join-item btn"
+              type="radio"
+              name="window"
+              value="30s"
+              aria-label="30s"
+              disabled={isLoading}
+              checked={chartConfig.window === "30s"}
+              onChange={handleWindowChange}
+            />
+            <input
+              className="join-item btn"
+              type="radio"
+              name="window"
+              value="1m"
+              aria-label="1m"
+              disabled={isLoading}
+              checked={chartConfig.window === "1m"}
+              onChange={handleWindowChange}
+            />
+            <input
+              className="join-item btn"
+              type="radio"
+              name="window"
+              value="5m"
+              aria-label="5m"
+              disabled={isLoading}
+              checked={chartConfig.window === "5m"}
+              onChange={handleWindowChange}
+            />
+          </div>
+          <div className="join ml-10">
+            <label className="input rounded-l-lg border-0 font-bold">
+              Function
+            </label>
+            <input
+              className="join-item btn"
+              type="radio"
+              name="agg-func"
+              value="avg_over_time"
+              aria-label="Avg"
+              disabled={isLoading}
+              checked={chartConfig.aggFunc === "avg_over_time"}
+              onChange={handleFuncChange}
+            />
+            <input
+              className="join-item btn"
+              type="radio"
+              name="agg-func"
+              value="max_over_time"
+              aria-label="Max"
+              disabled={isLoading}
+              checked={chartConfig.aggFunc === "max_over_time"}
+              onChange={handleFuncChange}
+            />
+          </div>
         </div>
-        <div className="join ml-10">
-          <label className="input rounded-l-lg border-0 font-bold">
-            Window
-          </label>
-          <input
-            className="join-item btn"
-            type="radio"
-            name="window"
-            value="30s"
-            aria-label="30s"
-            disabled={isLoading}
-            checked={chartConfig.window === "30s"}
-            onChange={handleWindowChange}
+        {data && (
+          <ThermalChart
+            labels={data.data.result[0].values.map((v) => {
+              const ts = dayjs.unix(v[0]);
+              return `${ts.hour()}:${ts.minute()}:${ts.second()}`;
+            })}
+            data={data.data.result[0].values.map((v) => Number(v[1]))}
           />
-          <input
-            className="join-item btn"
-            type="radio"
-            name="window"
-            value="1m"
-            aria-label="1m"
-            disabled={isLoading}
-            checked={chartConfig.window === "1m"}
-            onChange={handleWindowChange}
-          />
-          <input
-            className="join-item btn"
-            type="radio"
-            name="window"
-            value="5m"
-            aria-label="5m"
-            disabled={isLoading}
-            checked={chartConfig.window === "5m"}
-            onChange={handleWindowChange}
-          />
-        </div>
-        <div className="join ml-10">
-          <label className="input rounded-l-lg border-0 font-bold">
-            Function
-          </label>
-          <input
-            className="join-item btn"
-            type="radio"
-            name="agg-func"
-            value="avg_over_time"
-            aria-label="Avg"
-            disabled={isLoading}
-            checked={chartConfig.aggFunc === "avg_over_time"}
-            onChange={handleFuncChange}
-          />
-          <input
-            className="join-item btn"
-            type="radio"
-            name="agg-func"
-            value="max_over_time"
-            aria-label="Max"
-            disabled={isLoading}
-            checked={chartConfig.aggFunc === "max_over_time"}
-            onChange={handleFuncChange}
-          />
-        </div>
+        )}
       </div>
-      {data && (
-        <ThermalChart
-          labels={data.data.result[0].values.map((v) => {
-            const ts = dayjs.unix(v[0]);
-            return `${ts.hour()}:${ts.minute()}:${ts.second()}`;
-          })}
-          data={data.data.result[0].values.map((v) => Number(v[1]))}
-        />
-      )}
-    </div>
+      <DataRangeDropdown
+        id="custom-dr-popover"
+        positionAnchor="--custom-dr-anchor"
+        onApply={(s) => {
+          setChartConfig({
+            ...chartConfig,
+            start: s.start,
+            end: s.end,
+            range: "Custom",
+          });
+        }}
+      />
+    </>
   );
 };
