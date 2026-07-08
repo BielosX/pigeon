782574d588c1d2f2697d2ade8ed949bb99768b80
Mateusz Biel <matbiel@gmail.com>
2026-07-08 21:06:36 +0200

    Use react-hook-form for DataRangeDropdown

--- ui/src/pages/SystemCharts/components/DataRangeDropdown.tsx
+++ ui/src/pages/SystemCharts/components/DataRangeDropdown.tsx
@@ -1,10 +1,14 @@
 import { DayPicker } from "@daypicker/react";
 import { type DateRange } from "@daypicker/react";
-import { useRef, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import dayjs from "dayjs";
+import {
+  type SubmitHandler,
+  useForm,
+  type ValidateResult,
+} from "react-hook-form";
 
-interface DataRangeDropdownState {
-  selected: DateRange | undefined;
+interface FormInputs {
   timeStart: string;
   timeEnd: string;
 }
@@ -20,19 +24,24 @@ interface DataRangeDropdownProps {
   onApply: (state: DataRangeDropdownOutput) => void;
 }
 
-const toOutput = (state: DataRangeDropdownState): DataRangeDropdownOutput => {
-  const [startHour, startMinute] = state.timeStart
-    .split(":")
-    .map((t) => parseInt(t));
-  const [endHour, endMinute] = state.timeEnd.split(":").map((t) => parseInt(t));
-  if (!state.selected) {
+const parseTime = (time: string): number[] => {
+  return time.split(":").map((t) => parseInt(t));
+};
+
+const toOutput = (
+  input: FormInputs,
+  selected?: DateRange,
+): DataRangeDropdownOutput => {
+  const [startHour, startMinute] = parseTime(input.timeStart);
+  const [endHour, endMinute] = parseTime(input.timeEnd);
+  if (!selected) {
     return {
       start: dayjs().hour(startHour).minute(startMinute),
       end: dayjs().hour(endHour).minute(endMinute),
     };
   }
-  const selectedFrom = state.selected.from!;
-  const selectedTo = state.selected.to!;
+  const selectedFrom = selected.from!;
+  const selectedTo = selected.to!;
   return {
     start: dayjs(selectedFrom).hour(startHour).minute(startMinute),
     end: dayjs(selectedTo).hour(endHour).minute(endMinute),
@@ -44,19 +53,40 @@ export const DataRangeDropdown = ({
   positionAnchor,
   onApply,
 }: DataRangeDropdownProps) => {
-  const [state, setState] = useState<DataRangeDropdownState>(() => {
-    const now = dayjs();
-    const formatted = now.format("HH:mm");
-    return {
-      selected: {
-        from: now.toDate(),
-        to: now.toDate(),
-      },
-      timeStart: formatted,
-      timeEnd: formatted,
-    };
-  });
+  const [selected, setSelected] = useState<DateRange | undefined>();
   const ref = useRef<HTMLDivElement | null>(null);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInputs>();
+  const EndBeforeStartMsg = "End is before Start";
+
+  const onSubmit: SubmitHandler<FormInputs> = useCallback(
+    (data) => {
+      onApply(toOutput(data, selected));
+      ref.current?.hidePopover();
+    },
+    [selected],
+  );
+
+  const validateTimeEnd = useCallback(
+    (value: string, formValues: FormInputs): ValidateResult => {
+      const [startHour, startMinute] = parseTime(formValues.timeStart);
+      const [endHour, endMinute] = parseTime(value);
+      if (!selected || selected.from === selected.to) {
+        if (startHour > endHour) {
+          return EndBeforeStartMsg;
+        }
+        if (startHour === endHour && startMinute > endMinute) {
+          return EndBeforeStartMsg;
+        }
+      }
+      return true;
+    },
+    [selected],
+  );
+
   return (
     <div
       id={id}
@@ -70,46 +100,53 @@ export const DataRangeDropdown = ({
       <DayPicker
         animate
         mode="range"
-        selected={state.selected}
-        onSelect={(s) =>
-          setState({
-            ...state,
-            selected: s,
-          })
-        }
+        selected={selected}
+        onSelect={(s) => setSelected(s)}
       />
-      <label className="input mb-2 mt-2 bg-white">
-        <span className="label">Start</span>
-        <input
-          type="time"
-          className="input"
-          value={state.timeStart}
-          onInput={(e) => setState({ ...state, timeStart: e.data })}
-        />
-      </label>
-      <label className="input bg-white">
-        <span className="label">End&nbsp;</span>
-        <input
-          type="time"
-          className="input"
-          value={state.timeEnd}
-          onInput={(e) => setState({ ...state, timeEnd: e.data })}
-        />
-      </label>
-      <div className="flex flex-row justify-end mt-6">
-        <button className="btn mr-2" onClick={() => ref.current?.hidePopover()}>
-          Cancel
-        </button>
-        <button
-          className="btn btn-primary"
-          onClick={() => {
-            onApply(toOutput(state));
-            ref.current?.hidePopover();
-          }}
-        >
-          Apply
-        </button>
-      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <fieldset className="fieldset">
+          <label className="input mt-2 bg-white">
+            <span className="label">Start</span>
+            <input
+              type="time"
+              className="input"
+              {...register("timeStart", { required: "Start is required" })}
+            />
+          </label>
+          <p
+            className={`text-error ${!errors.timeStart ? "hidden" : "visible"}`}
+          >
+            {errors.timeStart?.message}
+          </p>
+        </fieldset>
+        <fieldset className="fieldset">
+          <label className="input bg-white">
+            <span className="label">End&nbsp;</span>
+            <input
+              type="time"
+              className="input"
+              {...register("timeEnd", {
+                required: "End is required",
+                validate: validateTimeEnd,
+              })}
+            />
+          </label>
+          <p className={`text-error ${!errors.timeEnd ? "hidden" : "visible"}`}>
+            {errors.timeEnd?.message}
+          </p>
+        </fieldset>
+        <div className="flex flex-row justify-end mt-6">
+          <button
+            className="btn mr-2"
+            onClick={() => ref.current?.hidePopover()}
+          >
+            Cancel
+          </button>
+          <button type="submit" className="btn btn-primary">
+            Apply
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
